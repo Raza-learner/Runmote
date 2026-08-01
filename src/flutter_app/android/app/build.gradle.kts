a1b5e471699163cf86cc1a6d69ca1aa634b5c7f9
Raza-learner <mohammadrazashaikh59@gmail.com>
2026-08-01 10:46:03 +0530

    Runmote logo fic

--- src/flutter_app/android/app/build.gradle.kts
+++ src/flutter_app/android/app/build.gradle.kts
@@ -2,11 +2,28 @@ plugins {
     id("com.android.application")
     id("dev.flutter.flutter-gradle-plugin")
 }
+import java.util.Properties
+        import java.io.FileInputStream
 
+val keystoreProperties = Properties()
+val keystorePropertiesFile = rootProject.file("key.properties")
+
+if (keystorePropertiesFile.exists()) {
+    keystoreProperties.load(FileInputStream(keystorePropertiesFile))
+}
 android {
     namespace = "com.acp.acp_remote"
     compileSdk = 36
     ndkVersion = flutter.ndkVersion
+
+    signingConfigs {
+        create("release") {
+            storeFile = file(keystoreProperties["storeFile"] as String)
+            storePassword = keystoreProperties["storePassword"] as String
+            keyAlias = keystoreProperties["keyAlias"] as String
+            keyPassword = keystoreProperties["keyPassword"] as String
+        }
+    }
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
@@ -14,7 +31,9 @@ android {
     }
 
     defaultConfig {
-        applicationId = "com.acp.acp_remote"
+        applicationId = "dev.runmote.app" +
+                "" +
+                ""
         minSdk = flutter.minSdkVersion
         targetSdk = 36
         versionCode = flutter.versionCode
@@ -23,7 +42,7 @@ android {
 
     buildTypes {
         release {
-            signingConfig = signingConfigs.getByName("debug")
+            signingConfig = signingConfigs.getByName("release")
             isMinifyEnabled = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
